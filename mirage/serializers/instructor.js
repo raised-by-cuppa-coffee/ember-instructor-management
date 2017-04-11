b334eb0a065239cc77bd53fecd4da4b25e7d802d
Adam Gribble <adam@thehumangeo.com>
2017-04-11 16:23:22 +0100

    Fixed minor bug with meta response

--- mirage/serializers/instructor.js
+++ mirage/serializers/instructor.js
@@ -4,9 +4,11 @@ export default JSONAPISerializer.extend({
   serialize(object, request) {
     let json = JSONAPISerializer.prototype.serialize.apply(this, arguments);
 
-    json.meta = {
-      total: object.meta.total
-    };
+    if (object.meta) {
+      json.meta = {
+        total: object.meta.total
+      };
+    }
 
     return json;
   }
