636f9e5b00cee9c0eba5308de373d550b35baa3d
Adam Gribble <adam@thehumangeo.com>
2017-04-11 16:32:45 +0100

    Added surge integration and temp config

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,10 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // production stuff here
+    // Only keep this while deploying to surge
+    ENV['ember-cli-mirage'] = {
+      enabled: true
+    };
   }
 
   return ENV;
