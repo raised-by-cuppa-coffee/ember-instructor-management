c154e5ca7da9d398c019a5efb88cf73b6c96f870
Adam Gribble <adam@thehumangeo.com>
2017-04-11 16:20:18 +0100

    Search with enter key as well

--- app/components/cuppa-search-bar/component.js
+++ app/components/cuppa-search-bar/component.js
@@ -6,15 +6,29 @@ export default Component.extend({
   classNames: ['ui huge fluid icon input'],
   classNameBindings: ['isSearching:loading'],
 
+  _debounceTimer: null,
   isSearching: false,
   q: '',
 
   keyUp(e) {
     let q = this.get('q');
+
+    // If query length is over 3, search when user stops typing
     if (q.length > 3) {
-      run.debounce(this, this._search, 1000);
+      this.set('_debounceTimer', run.debounce(this, this._search, 1000));
     } else if (q.length === 0) {
-      run.debounce(this, this._search, 150, true);
+      // If query is empty, immediately search for all
+      run.cancel(this.get('_debounceTimer'));
+      this._search();
+    }
+  },
+  keyPress(e) {
+    let { keyCode } = e;
+
+    // If enter key is hit, query immediately
+    if (keyCode === 13) {
+      run.cancel(this.get('_debounceTimer'));
+      this._search();
     }
   },
   
