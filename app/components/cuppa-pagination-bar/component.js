e43631f7fa8813fe08919e95ab2a4ce84ec59c86
Adam Gribble <adam@thehumangeo.com>
2017-04-22 16:46:19 +0100

    Cleaned up pagination-bar component

--- app/components/cuppa-pagination-bar/component.js
+++ app/components/cuppa-pagination-bar/component.js
@@ -5,19 +5,22 @@ const { computed, Component } = Ember;
 export default Component.extend({
   classNames: ['ui', 'basic', 'buttons'],
 
-  activePage: 1,
+  activePage: 0,
   totalPages: 0,
 
+  activeBtn: computed('activePage', function() {
+    return this.get('activePage') + 1;
+  }),
   paginateButtons: computed('activePage', 'totalPages', function() {
     // Grab the active page and total pages
     let totalPages = this.get('totalPages');
-    let centerIndex = this.get('activePage') - 1;
+    let centerIndex = this.get('activePage');
 
     // Calculate left and right bounds, if either side doesn't reach full length
     // try to apply extras to the opposite side
     let leftIndex = (centerIndex - 5) > 0 ? centerIndex - 5 : 0;
-    let rightIndex = (centerIndex + 5) <= totalPages - 1 ? centerIndex + 5 : totalPages - 1;
-    let extraLeft = rightIndex === totalPages - 1 ? ((centerIndex + 5) - (totalPages - 1)) : 0;
+    let rightIndex = (centerIndex + 5) <= totalPages ? centerIndex + 5 : totalPages;
+    let extraLeft = rightIndex === totalPages ? ((centerIndex + 5) - totalPages) : 0;
     let extraRight = leftIndex === 0 ? Math.abs(centerIndex - 5) : 0;
 
     // Apply extra pages to either side if there's room
@@ -25,7 +28,7 @@ export default Component.extend({
     rightIndex = rightIndex + extraRight <= totalPages ? rightIndex + extraRight : totalPages;
 
     // Return an array starting at the left bound and ending at the right
-    let arrayLength = (rightIndex - leftIndex) + 1 >= 11 ? 11 : (rightIndex - leftIndex) + 1;
+    let arrayLength = (rightIndex - leftIndex) >= 11 ? 11 : (rightIndex - leftIndex);
     let buttonArray = [];
     let n = 0;
 
@@ -47,12 +50,12 @@ export default Component.extend({
       this.get('onPaginate')(page);
     },
     nextPage() {
-      if (this.get('activePage') + 1 <= this.get('totalPages')) {
+      if (this.get('activePage') <= this.get('totalPages')) {
         this.send('activatePage', this.get('activePage') + 1);
       }
     },
     prevPage() {
-      if (this.get('activePage') - 1 >= 1) {
+      if (this.get('activePage') >= 0) {
         this.send('activatePage', this.get('activePage') - 1);
       }
     }
